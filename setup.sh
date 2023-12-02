f559f987ebc9882254a5eba1c03ff5b40d9d3fd1
Ben Halstead <benhalstd@gmail.com>
2023-12-02 20:37:08 +1300

    add ripgrep

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ sudo apt-get install nodejs npm
 sudo apt-get install neovim
 sudo apt-get install python-tk python3-tk tk-dev
 sudo apt-get install fd-find
+sudo apt-get install ripgrep
 
 username_home=$HOME
 if [ ! -d "$username_home/.asdf" ]; then
