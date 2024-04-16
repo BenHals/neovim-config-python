7ea79394d8b77782e30c56c040b0d710d2776565
Ben Halstead <benhalstd@gmail.com>
2024-04-16 14:10:46 +1200

    colorscheme

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -14,27 +14,42 @@ return {
             })
 
             vim.cmd.colorscheme "catppuccin"
+            vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = true })
+            vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = true })
+            vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F', bold = true })
         end
     },
     {
         'rose-pine/neovim',
         name = 'rose-pine',
         priority = 1000,
-        opts = {
-            highlight_groups = {
-                Cursor = { fg = '#ffffff', bg = '#ffffff' }
-            },
-            variant = "auto",
-            styles = {
-                transparency = true
+        config = function()
+            local opts = {
+                highlight_groups = {
+                    Cursor = { fg = '#ffffff', bg = '#ffffff' }
+                },
+                variant = "auto",
+                styles = {
+                    transparency = true
+                }
             }
-        },
+            require("rose-pine").setup(opts)
+            vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = true })
+            vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = true })
+            vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F', bold = true })
+        end
     },
     {
         "eldritch-theme/eldritch.nvim",
         lazy = false,
         priority = 1000,
-        opts = {
-        },
+        config = function()
+            local opts = {}
+            require("eldritch").setup(opts)
+            vim.cmd.colorscheme "eldritch"
+            vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = true })
+            vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = true })
+            vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F', bold = true })
+        end
     },
 }
