3a959ddcb1ba419b965773d2eab5035aacd46468
Ben Halstead <benhalstd@gmail.com>
2024-10-30 21:42:42 +1300

    Clean

--- .config/nvim/lua/custom/plugins/lsp_config.lua
+++ .config/nvim/lua/custom/plugins/lsp_config.lua
@@ -173,30 +173,13 @@ return {
         rust_analyzer = {},
         ts_ls = {},
         html = { filetypes = { 'html', 'twig', 'hbs' } },
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
@@ -240,10 +223,10 @@ return {
           end,
         },
       }
-      require('lspconfig')['terraformls'].setup {
-        capabilities = capabilities,
-        filetypes = { 'tf', 'tfvars' },
-      }
+      -- require('lspconfig')['terraformls'].setup {
+      --   capabilities = capabilities,
+      --   filetypes = { 'tf', 'tfvars' },
+      -- }
 
       local null_ls = require 'null-ls'
 
