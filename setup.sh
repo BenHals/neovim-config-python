eca3223ca3d0acad8e971a51ce5333758fb6d832
Ben Halstead <benhalstd@gmail.com>
2024-12-30 10:23:55 +1300

    rearrange

--- setup.sh
+++ setup.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 unameOut="$(uname -s)"
-echo $unameOut
+echo "Machine: ${unameOut}"
 
-
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install build-essential
-sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc liblzma-dev lzma
-sudo apt-get install nodejs npm
-sudo apt-get install neovim
-sudo apt-get install python-tk python3-tk tk-dev
-sudo apt-get install fd-find
-sudo apt-get install ripgrep
-sudo apt-get install stow
+if [ "${unameOut}" == "Darwin" ]; then
+  echo "Setup on MacOS not implemented. Install neovim, tmux, starship, ghostty, python, fd, ripgrep and stow!"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  sudo add-apt-repository ppa:neovim-ppa/unstable
+  sudo apt-get update
+  sudo apt-get install build-essential
+  sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc liblzma-dev lzma
+  sudo apt-get install nodejs npm
+  sudo apt-get install neovim
+  sudo apt-get install python-tk python3-tk tk-dev
+  sudo apt-get install fd-find
+  sudo apt-get install ripgrep
+  sudo apt-get install stow
+else
+  echo "Setup on Windows not implemented. Install neovim, tmux, starship, ghostty, python, fd, ripgrep and stow!"
+fi
 
 username_home=$HOME
 
@@ -27,7 +32,8 @@ stow tmux
 stow starship
 stow iterm2
 stow ghostty
+stow funcs
 
 
 source $username_home/.bashrc
-tmux source $username_home/.tmux.conf
+tmux source $username_home/.config/tmux/tmux.conf
